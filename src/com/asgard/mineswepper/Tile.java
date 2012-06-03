1b8d5de580ae0f15dc2cd32602625d1f388d0c6d
Gabriel Hidasy <gabrielhidasy@gmail.com>
2012-06-02 23:48:15 -0300

    mexendo com o rand já, ainda sem SDK

--- src/com/asgard/mineswepper/Tile.java
+++ src/com/asgard/mineswepper/Tile.java
@@ -3,36 +3,32 @@ package com.minas_explosivas;
 import android.widget.ImageButton;
 
 public class Tile {
-	private ImageButton button;
-	private boolean minePresent;
-	public int px,py;
+    private ImageButton button;
+    private boolean minePresent;
+    public int px,py;
 	
-	public boolean isParachutePresent() {
-		return parachutePresent;
-	}
 
-	public void setParachutePresent(boolean parachutePresent) {
-		this.parachutePresent = parachutePresent;
-	}
-
-	public Tile(ImageButton button) {
-		this.button=button;
-		this.minePresent=false;
-	}
+    public Tile(ImageButton button) {
+	this.button=button;
+	this.minePresent=false;
+    }
 	
-	public ImageButton getButton() {
-		return button;
-	}
-	public void setButton(ImageButton button) {
-		this.button = button;
-	}
-	public boolean isMinePresent() {
-		return minePresent;
-	}
-	public void setMinePresent(boolean minePresent) {
-		this.minePresent = minePresent;
-	}
-
+    public ImageButton getButton() {
+	return button;
+    }
+    public void setButton(ImageButton button) {
+	this.button = button;
+    }
+    public void set_pos(int x, int y) {
+	this.px=x;
+	this.py=y;
+    }
+    public int get_posx() {
+	return px;
+    }
+    public void get_posy() {
+	return py;
+    }
 	
 
 }
--- src/com/asgard/mineswepper/matbombs.java
+++ src/com/asgard/mineswepper/matbombs.java
@@ -1,20 +1,39 @@
 package com.asgard.mineswepper;
 
+import java.util.Random;
+
 public class matbombs {
 	private int[][] mat;
+    private int x,y;
 	matbombs(int num, int dimx, int dimy) {
 		mat = new int[dimx][dimy];
+		x=dimx;
+		y=dimy;
+		Random rand = new Random(); 
 		for(int i=0;i<num;i++) {
-		//TODO usar rand pra encher essa merda
+		    mat[rand.nextInt(dimx)][rand.nextInt(dimy)]=1;
 		}
 	}
 
-	public static boolean check(String string, String string2) {
+	public static boolean check(Tile name) {
+	    if(mat[name.px][name.py]==1) {
+		return true;
+	    }
+	    else {
 		return false;
+	    }
 	}
 
-	public int bombsAside(String string, String string2) {
-		return 6;
-		
+	public int bombsAside(Tile name) {
+	    int count = 0;
+	    for(int i=name.px-1; i<=name.px+1;i++) {
+		for(int j=name.py-1;j<=name.py;j++) {
+		    if(i>0 && j>0 && i<x && j<y) {
+			if(mat[i][j]==1)
+			    count++;
+		    }
+		}
+	    }	
+	    return count;
 	}
 }
